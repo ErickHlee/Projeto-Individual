9649a76f93930d02498b92dad086f3ca440d6f8b
ErickHlee <erick.hlee@sptech.school>
2024-11-14 22:58:27 -0300

    Update: html, css

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -24,3 +24,5 @@ prev.addEventListener('click', function () {
     let items = document.querySelectorAll('.item');
     document.querySelector('.slide').prepend(items[items.length - 1]);
 });
+
+document.body.style.height = window.innerHeight + 'px'
